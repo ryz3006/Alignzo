8f5cb866a32d5cb400dc3876fa52d17967097a66
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-10 16:12:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This base path must exactly match your GitHub repository name.
   base: '/Alignzo/',
   plugins: [react()],
 })
