f2e35b397430b95febfc7621a4cd33622e9c3af8
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-11 09:51:05 +0530

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 
 const ThemeContext = createContext();
-
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
