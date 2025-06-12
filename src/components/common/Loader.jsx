0a264f6709fd5d3461abb7ed3b85d63d20fea471
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-12 12:18:47 +0530

    Create Loader.jsx

---
+++ src/components/common/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="banter-loader">
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
