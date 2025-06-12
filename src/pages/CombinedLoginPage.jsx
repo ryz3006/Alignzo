3b8a248dedeb6b41bbaa2723cce87454aefcbcb2
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-12 17:47:16 +0530

    Update CombinedLoginPage.jsx

--- src/pages/CombinedLoginPage.jsx
+++ src/pages/CombinedLoginPage.jsx
@@ -18,7 +18,7 @@ const CombinedLoginPage = () => {
   const [activeTab, setActiveTab] = useState('user');
   const [error, setError] = useState('');
   const { isAppLoading, setIsAppLoading } = useAuth();
-  const [username, setUsername] = useState('admin');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
