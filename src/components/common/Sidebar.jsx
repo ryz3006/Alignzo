4036e13a01a8108957a78b8fca69a72b13b78047
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-12 19:13:10 +0530

    Update Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -16,7 +16,21 @@ const TrackerIcon = () => <svg style={{height: '1.5rem', width: '1.5rem'}} fill=
 const ProfileIcon = () => <svg style={{height: '1.5rem', width: '1.5rem'}} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" /></svg>;
 const ScheduleIcon = () => <svg style={{height: '1.5rem', width: '1.5rem'}} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008z" /></svg>;
 const DocumentIcon = () => <svg style={{height: '1.5rem', width: '1.5rem'}} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>;
-const ProjectSettingsIcon = () => <svg style={{height: '1.5rem', width: '1.5rem'}} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" /></svg>;
+const ProjectSettingsIcon = () => (
+  <svg
+    style={{ height: '1.5rem', width: '1.5rem' }}
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.25 1.5c.621 0 1.125.504 1.125 1.125v.326a8.988 8.988 0 013.591 1.49l.23-.23a1.125 1.125 0 111.591 1.591l-.23.23a8.988 8.988 0 011.49 3.591h.326c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125h-.326a8.988 8.988 0 01-1.49 3.591l.23.23a1.125 1.125 0 11-1.591 1.591l-.23-.23a8.988 8.988 0 01-3.591 1.49v.326c0 .621-.504 1.125-1.125 1.125h-1.5c-.621 0-1.125-.504-1.125-1.125v-.326a8.988 8.988 0 01-3.591-1.49l-.23.23a1.125 1.125 0 11-1.591-1.591l.23-.23a8.988 8.988 0 01-1.49-3.591H1.5c-.621 0-1.125-.504-1.125-1.125v-1.5c0-.621.504-1.125 1.125-1.125h.326a8.988 8.988 0 011.49-3.591l-.23-.23a1.125 1.125 0 011.591-1.591l.23.23a8.988 8.988 0 013.591-1.49v-.326c0-.621.504-1.125 1.125-1.125h1.5zM12 15a3 3 0 100-6 3 3 0 000 6z"
+    />
+  </svg>
+);
 
 
 const Sidebar = ({ isMobileOpen, setMobileOpen }) => {
