93205dfd95629b2269a93de07cfc75028d010638
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-12 18:33:29 +0530

    Update and rename DocumentsPage.jsx to DocumentRepositoryPage.jsx

---
+++ src/pages/public/DocumentRepositoryPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const DocumentRepositoryPage = () => { return <div className="text-strong">Document Repository Page Content</div> };
+export default DocumentRepositoryPage;
