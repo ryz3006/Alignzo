f551cb436aede88e49dce5281286404446b6b731
ryz3006 <32481216+ryz3006@users.noreply.github.com>
2025-06-12 18:47:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { ProjectProvider } from './contexts/ProjectContext.jsx'; // Import the new provider
 import './neumorphism.css';
-import './loader.css'; // Import new loader styles
+import './loader.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <ThemeProvider>
         <AuthProvider>
-          <App />
+          <ProjectProvider>
+            <App />
+          </ProjectProvider>
         </AuthProvider>
       </ThemeProvider>
     </HashRouter>
